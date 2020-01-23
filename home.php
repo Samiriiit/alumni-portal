f4bd41a0ca1a46a8ddd78b6c9ceb2f77f8151ec6
Samir Kumar <51961736+Samiriiit@users.noreply.github.com>
2020-01-24 01:26:05 +0530

    Add files via upload
    added navbar with crousel

---
+++ home.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+ 
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+<title>Alumini Portal</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+
+ <link href="../css/bootstrap.min.css "rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="http://code.jquery.com/jquery-3.3.1.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/css/bootstrap.min.css">
+    <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <style>
+ul li {
+      display: inline-block;
+      font-weight:bold;
+	  
+}
+nav>img{ 
+      height:50px;  
+      width:90px;
+      float: left;  
+      color: white;
+}
+.v1{
+      float: right; 
+      right:0;
+}
+nav>ul li a {
+      color:#FFF;
+      text-decoration: none;
+      padding: 15px;
+      display: block;
+}
+nav>ul li a:hover {
+      color:#FFF;
+      text-decoration: none;
+      padding: 15px;
+      display: block;
+}
+nav>ul li:hover {      
+      color:#333;
+      background:#991234;
+      text-decoration:none;
+}
+nav>ul ul {
+      position: absolute;
+      min-width: 200px;
+      background: #e46226;
+      display: none;
+}
+nav>ul ul li {
+      display: block;
+      background: #5d4970;
+}
+nav>ul li:hover ul {
+      display: block;
+}
+nav>ul li i {
+      color: #FFF;
+      float: right;
+      padding-left: 20px;
+}
+nav div {
+      
+      color: #FFF;
+font-size: 24px;
+      padding: 0.6em;
+      cursor: pointer;
+      display: none;
+	  
+}
+nav{
+	width:100%;
+	background:#5d4970;
+	height:55px;
+	position: fixed;
+      top:0;
+      z-index:20;
+      border-bottom:#f77c9b solid 4px;
+      height:60px;
+}
+@media(max-width: 768px) {
+      nav{
+	      width:100%;
+      }
+	  nav div {
+            display: block;
+      }
+ nav>ul 
+ {
+      display: none;
+      position: static;
+      background-color: #5d4970;
+      float:left;
+      width:100%;
+}
+nav>ul li {
+      display: block;
+}
+nav>ul ul {
+      position: static;
+      background-color:#5d4970;
+}}
+
+
+
+.carousel-button:hover{ background-color: red; 
+            color: white ;}
+
+
+.carousel-item img{
+      height:100%;
+      width:100%;
+      
+      }
+.carousel-control-next-icon,.carousel-control-prev-icon{
+      height:10%;
+      width:10%;
+}
+.carousel-caption{
+      background-color: black;
+      color:white;
+      width:100%;
+      left:0;
+
+}
+.carousel-indicator{
+      margin-bottom:20%;
+}
+.carousel-button{border-radius:50px;}
+ @media(min-width: 768px) {
+      .carousel-button{
+            background-color:#f2ecb3; 
+            color: #403a02;
+            font-size: 20px;
+      }
+      .carousel-caption {
+              background-color: transparent;
+            color:#d7f5ec;
+            width:100%;
+            left:0;
+            font-size: 28px;
+      }
+  }
+  @media(max-width: 768px) {
+      .carousel-caption{
+            background-color: #5d4970;
+            color:white;
+            width:100%;
+            left:0px;
+            font-size: 19px;
+            bottom:0;            
+            margin:0px;
+            padding:0%;
+            padding-bottom:5%;
+            line-height:100%;
+            
+      }
+      .carousel-button{
+      background-color:#FFF; 
+      color:#e46226;
+      font-size: 19px;
+      line-height:50%;
+      padding:3%;
+      margin-bottom:8px;
+      }
+      .carousel-indicators
+      {
+            visibility:hidden;
+      }
+      .info-grids
+      {
+            background-size: cover; 
+            background-repeat: no-repeat;
+            width: 100%;
+	      height: 100%;
+      }
+      .info-grids-para
+      {
+            font-size:16px; 
+      }
+      .info-grids-btn
+      {
+            background-color:#e46226; 
+            color:#FFF; 
+            font-size:16px; 
+            font-weight:bold; 
+            margin-bottom:1%;
+      }
+  } 
+</style>
+
+</head>
+<body>
+
+<nav>            
+         <a href="index.php"><img src="goa.png" style="margin-top:-2px; height:55px; width:88px; float:left;"></a>
+                        <div style="float:right; right:0;">
+                              <i class="fa fa-bars navbar-right"></i>
+                        </div>
+                        <ul  style="float:right;">
+                        
+                              <li><a href="/">About Us</a></li>
+                              <li><a href="#">Directory</a>
+                                    
+                              </li>
+                              <li><a href="#">Campuses</a>
+                                  
+                              </li>
+                              <li><a href="#">Events</a>
+                                    
+                              </li>
+                              <li><a href="#">Newsletter</a></li>
+                              <li><a href="#">FAQ</a>
+                      
+                              </li>
+                              <li><a href="#">Login</a></li>
+                        </ul>
+                    </nav><div style="height:60px;">
+</div>
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel"style="height:400px; padding:0px; margin:0px; top:0;">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+          </ol>
+      
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" style="height:400px; padding:0px; margin:0px; top:0;">
+          <div class="item active">
+              <img src="1.jpg" alt="1" style="height:400px;width:100%; top:0;">
+              <br/>
+              <div class="carousel-caption">
+   <p><b>Welcome to the Alumni Network! </b></p>
+                <a href="joinus.php"><button class="btn carousel-button" >Join the network! </button></a>
+              </div>
+            </div>
+      
+            <div class="item">
+            <img src="2.JPG" alt="2" style="width:100%;height:400px; top:0;">
+              <br/>
+              <div class="carousel-caption">
+ <p><b>Welcome to the Alumni Network! </b></p>
+               
+               <a href="joinus.php"> <button class="btn carousel-button" >Join the network!</button></a>
+                
+                
+              </div>
+            </div>
+          
+            <div class="item">
+            <img src="3.jpg" alt="3" style="width:100%;height:400px; top:0;">
+              <br/>
+              <div class="carousel-caption">
+ <p><b>Welcome to the Alumni Network!</b> </p>
+                <a href="joinus.php"><button class="btn carousel-button" >Join the network!</button></a>
+               
+                
+              </div>
+            </div>
+
+            <div class="item">
+            <img src="4.JPG" alt="4" style="width:100%;height:400px; top:0;">
+              <br/>
+              <div class="carousel-caption">
+         
+        <p><b>Welcome to the Alumni Network!</b> </p>
+                <a href="joinus.php"><button class="btn carousel-button" >Join the network!</button></a>
+               
+                
+              </div>
+            </div>
+
+            <div class="item">
+            <img src="5.jpg" alt="5" style="width:100%;height:400px; top:0;">
+              <br/>
+              <div class="carousel-caption">
+ <p><b>Welcome to the Alumni Network!</b> </p>
+                 <a href="joinus.php"><button class="btn carousel-button" >Join the network!</button></a>
+               
+                
+              </div>
+            </div>
+      
+            <div class="item">
+            <img src="6.jpg" alt="5" style="width:100%;height:400px; top:0;">
+              <br/>
+              <div class="carousel-caption">
+ <p><b>Welcome to the Alumni Network!</b> </p>
+                
+                <a href="joinus.php"> <button class="btn carousel-button" >Join the network!</button></a>
+                 
+                
+              </div>
+            </div>
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+	  </body>
+
+
+
+ <script type="text/javascript">
+      $("nav div").click(function() {
+            $("ul").slideToggle();
+            $("ul ul").css("display", "none");
+      });
+      $("ul li").click(function() {
+            $("ul ul").slideUp();
+            $(this).find('ul').slideToggle();
+      });
+ $(window).resize(function() {
+            if($(window).width() > 768) {
+                  $("ul").removeAttr('style');
+            }
+      });
+      </script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+     
+
+
+<script
+  src="https://code.jquery.com/jquery-1.7.2.min.js"
+  integrity="sha256-R7aNzoy2gFrVs+pNJ6+SokH04ppcEqJ0yFLkNGoFALQ="
+ crossorigin="anonymous">
+</script>
+
+
+
+
+</html>
